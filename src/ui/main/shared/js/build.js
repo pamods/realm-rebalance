f888bb5cfe8321b79cff4f2aaae3c0eed96380fd
YourlocalmadSci <simon.c.lumley@gmail.com>
2014-07-17 18:41:44 +0100

    fusillade AA

--- src/ui/main/shared/js/build.js
+++ src/ui/main/shared/js/build.js
@@ -50,7 +50,8 @@ function BuildHotkeyModel() {
             "/pa/units/land/tank_light_laser/tank_light_laser.json": ["vehicle", 11],
             "/pa/units/land/tank_armor/tank_armor.json": ["vehicle", 12],
             "/pa/units/land/land_scout/land_scout.json": ["vehicle", 13],
-            "/pa/units/land/aa_missile_vehicle/aa_missile_vehicle.json": ["vehicle", 14],    
+            "/pa/units/land/aa_missile_vehicle/aa_missile_vehicle.json": ["vehicle", 14], 
+			"/pa/units/land/tank_flak_adv/tank_flak_adv.json": ["vehicle", 4],
 
             "/pa/units/land/fabrication_bot_adv/fabrication_bot_adv.json": ["bot", 5],   
             "/pa/units/land/assault_bot_adv/assault_bot_adv.json": ["bot", 6],
